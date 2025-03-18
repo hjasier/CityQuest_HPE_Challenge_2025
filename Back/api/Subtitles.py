23e2a4b2cc0a3a21ab76b6e017911948b42d943a
hjasier <asiaurre@gmail.com>
2025-03-18 21:05:07 +0100

    a

--- Back/api/Subtitles.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, request, jsonify
-import yt_dlp
-import re
-import random
-import string
-import requests
-import os
-
-subtitles_bp = Blueprint('subtitles', __name__)
--- Back/api/TextComposer.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, Response, Blueprint, request, jsonify
-from openai import OpenAI
-import os
-import time
-import re
-
-text_composer_bp = Blueprint('text_composer_bp', __name__)
