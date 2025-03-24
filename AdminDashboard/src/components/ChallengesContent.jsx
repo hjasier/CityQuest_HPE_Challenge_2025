21140b7d1677e54f8e43afa03e897a1cec6df820
hjasier <asiaurre@gmail.com>
2025-03-24 12:54:14 +0100

    fix

--- AdminDashboard/src/components/ChallengesContent.jsx
+++ AdminDashboard/src/components/ChallengesContent.jsx
@@ -1,11 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Search, Plus, Edit, Trash2, Filter, Settings, ChevronDown, Clock, Users } from 'lucide-react';
-import { createClient } from '@supabase/supabase-js';
+import { supabase } from '../hooks/supabaseClient';
 
-// Initialize Supabase client
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
 
 const ChallengesContent = () => {
   const [challenges, setChallenges] = useState([]);
