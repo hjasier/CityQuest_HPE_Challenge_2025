c3c2816619f5f127aa04d5f030932f9ba5032d1e
hjasier <asiaurre@gmail.com>
2025-03-24 13:04:15 +0100

    FIX toggle

--- AdminDashboard/src/components/ChallengesContent.jsx
+++ AdminDashboard/src/components/ChallengesContent.jsx
@@ -521,7 +521,7 @@ const ChallengesContent = () => {
 
           {/* Repeatable Toggle */}
           <div className="flex items-center">
-            <div className="relative inline-flex items-center cursor-pointer">
+            <label htmlFor="repeatable" className="relative inline-flex items-center cursor-pointer">
               <input 
                 type="checkbox" 
                 id="repeatable" 
@@ -531,7 +531,7 @@ const ChallengesContent = () => {
               />
               <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-500/20 dark:peer-focus:ring-blue-500/30 rounded-full peer dark:bg-gray-600 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-500"></div>
               <span className="ml-3 text-sm font-medium text-gray-700 dark:text-gray-300">Reto repetible</span>
-            </div>
+            </label>
           </div>
         </div>
       </div>
